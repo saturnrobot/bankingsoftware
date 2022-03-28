404ded80084309d599df09336ad02ad70dfa551f
q <diskofgrain.xyz>
2022-03-28 15:14:33 -0400

    removed bin folder

--- bin/path_config.h
+++
@@ -1,2 +0,0 @@
-#define RESOURCES_DIRECTORY "/home/q/Documents/Shared-School/Winter2022/COMP2501/assignments/assignment4/assets"
-#define AUDIO_DIRECTORY "/home/q/Documents/Shared-School/Winter2022/COMP2501/assignments/assignment4/assets/audio/"
